0c6be4e3b5833be6ab4f59f79290bc1dbdfcb815
djoker001 <34662153+djoker001@users.noreply.github.com>
2019-02-10 23:11:01 +0100

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -3,7 +3,7 @@ package Basics_in_Java;
 public class Arrays {
 
 	public static void main(String[] args) {
-	double[] array = {1.1, 2.33, 44, 0.55, 677777}  ;
+	double[] array = {1.1, 2.33, 44, 0.55, 677777, 899.9}  ;
 	
 	for(int i=0 ; i< array.length ; i++) {
 		
